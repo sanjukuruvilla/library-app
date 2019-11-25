18860f34dfce6afec2af131faf405bb885d45177
thelunatickid <57086804+thelunatickid@users.noreply.github.com>
2019-11-25 16:43:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,65 @@
 var express = require('express');
-var bodyparser=require('body-parser');
 var chalk = require('chalk');
 var path = require('path');
 
+var nav = [{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/books/addbooks',title:'Add Book'}]
+var bodyParser = require('body-parser');
+var authorsRouter = require('./src/routes/authorsrouter.js')(nav);
+var booksRouter = require('./src/routes/booksrouter.js')(nav);
+//var accountsRouter = require('./src/routes/accountsrouter.js')(accounts);
+
 var app = new express();
-var nav = [{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/books/addbooks',title:'addbooks'}]
 
-var booksRouter = require("./src/routes/booksrouter")(nav)
-
-app.use(bodyparser.urlencoded({extended:true}));
-app.use('/books',booksRouter);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/authors',authorsRouter); //AUTHOR
+app.use('/books',booksRouter); //BOOK
+//app.use('/accounts',accountsRouter); //ACCOUNTS
 app.use(express.static(path.join(__dirname,"public")));
-
 
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 
-
-
-
-// app.get('/',function(req,res){
-//     //res.send("Hello World 5000");
-//     res.sendFile(path.join(__dirname,"src/views/index.html"));
-// })
 app.get('/',function(req,res){
     res.render("index.ejs",{nav,title:'Library Management System'});
 })
 
+                                    /* ACCOUNTS */
 
+var accounts=[];
 
-// app.get("/books/:id",function(req,res){
-//     var i = req.params.id; 
-//     res.render("book.ejs",{nav,book:books[i]});
-// })
+    app.get('/accounts/login',(req,res)=>{
+        res.render("login.ejs",{title:'LOGIN'});
+    })
 
-app.get("/authors",function(req,res){
-    res.render("authors.ejs",{nav,title:'Authors'});
-})
+    app.get('/accounts/signup',(req,res)=>{
+        res.render('signup.ejs',{title:'SIGN UP'})
+    })
+
+    app.post('/accounts/validate-login',(req,res)=>{
+        var flag =-1;
+        var login = req.body;
+        console.log(login);
+        for(var i=0;i<accounts.length;i++){
+            if(login.username == accounts[i].username && login.password == accounts[i].password){
+                res.send("Logged into "+accounts[i].username);
+                flag = 0;
+            }
+        }
+         if(flag == -1){
+            res.send("Error")
+        }    
+    })
+
+    app.post('/accounts/validate-signup',(req,res)=>{
+        var details = req.body;
+        if(details.password == details.re_password){
+            accounts.push(details);
+        }
+        
+        console.log(accounts);
+        console.log(accounts.length);
+        res.render("index.ejs",{nav,title:'Library Management System'})
+    })
 
 app.listen(3000,function(){
     console.log(chalk.green("Listening to port ")+chalk.red(3000)+" .............")
